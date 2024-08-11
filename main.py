b7dac575c75dd85bed47e71b3939ff13620801eb
Henrijs Princis <henrijs.princis@inbox.lv>
2024-08-11 20:47:45 +0300

    fix typo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-from custom_trainer.custom_trainer import main as NTP_main
-from custom_trainer.evaluate_model import main as evaluate_main
+from training_and_evaluation.custom_trainer import main as NTP_main
+from training_and_evaluation.evaluate_model import main as evaluate_main
 
 with open("config.json") as f:
     config = json.load(f)
